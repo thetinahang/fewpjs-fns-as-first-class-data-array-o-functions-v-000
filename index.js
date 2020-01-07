8e7c47c18c68bf25d7181156957e070a3d0def3a
Tina Hang <mstinahang@gmail.com>
2020-01-07 04:32:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
 function leashDog(dogName, dogBreed) {
