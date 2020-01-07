da067e4673fac2d5e53b9131a96201f343c543fd
Tina Hang <mstinahang@gmail.com>
2020-01-07 04:29:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
+
+function leashDog(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed) {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
+console.log("Walk home with Byron the poodle");
+console.log("Unleash Byron the poodle");
